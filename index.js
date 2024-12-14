6d1d497b18d41998dcb140c1685c8cc02002220f
Suriya Prakash <sp.suriyaprakash21@gmail.com>
2024-12-15 00:20:42 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-function hello(){
-    const a = 0;
-    const b = 5;
-}
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+})
+app.post("/submit", (req, res) => {
+    res.render("partials/createblog.ejs");
+})
+app.post("/signin", (req, res) => {
+    res.render("partials/signin.ejs");
+})
+
