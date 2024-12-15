7487e1fd787f23a4a31991cc2718cac16c2c42fc
Suriya Prakash <sp.suriyaprakash21@gmail.com>
2024-12-16 01:09:20 +0530

    Viewing Schedeled Capsules updated

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post("/signup", async (req, res) => {
       res.send("Username already exists. Try logging in.");
     } else {
       const no = await db.query("INSERT INTO users (password, email, username) VALUES ($1, $2, $3);",[password, username, name]);
-      res.render("index.ejs");
+      res.render("index.ejs", { name: name});
     }
   } catch(err) {
     console.log(err);
@@ -48,12 +48,13 @@ app.post("/signup", async (req, res) => {
 app.post("/login", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+  const name = req.body.name;
   try{
     const checkexist = await db.query("SELECT * FROM users WHERE email = ($1);",[username]);
       if(checkexist.rows.length > 0){
         const pass = checkexist.rows[0];  
         if(pass.password == password){
-          res.render("index.ejs");
+          res.render("index.ejs", { name: pass.username});
         } else {
           res.send("Incorrect Password.")
         }
@@ -68,28 +69,22 @@ app.get("/card", (req, res) => {
     res.render("card.ejs", { heading: "New Post", submit: "Create Post" });
 });
 
-app.get("/posts/:id", (req, res) => {
-    console.log("hiiiiii");
-    const post = posts.find((p) => p.id === parseInt(req.params.id));
-    if (!post) return res.status(404).json({ message: "Post not found" });
-    res.json(post);
-});
-app.post("/create",(req,res)=>{
-    const newpost = {
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author,
-      Remail: req.body.usermail,
-      date: req.body.date,
-      time: req.body.date,
+app.post("/create", async (req,res)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    const author = req.body.composername;
+    const Remail = req.body.receiveremail;
+    const date = req.body.senddate;
+
+    try {
+        const no = await db.query("INSERT INTO content (title, message, username, receiver_email, date) VALUES ($1, $2, $3, $4, $5);",[title, content, author, Remail, date]);
+        res.render("index.ejs", {name: author});
+    } catch(err) {
+        console.log(err);
     }
-    lastId = newid;
-    posts.push(newpost);
-    res.status(201);
-    res.json(newpost);
 });
 
-app.patch("/posts/:id", (req, res) => {
+/*app.patch("/posts/:id", (req, res) => {
     const post = posts.find((p) => p.id === parseInt(req.params.id));
     if (!post) return res.status(404).json({ message: "Post not found" });
   
@@ -98,7 +93,22 @@ app.patch("/posts/:id", (req, res) => {
     if (req.body.author) post.author = req.body.author;
   
     res.json(post);
-});
+});*/
+
+app.get("/capsules", async (req, res) => {
+    try {
+      const dbresponse = await db.query("SELECT title, message, username, receiver_email, date FROM content;");
+      let response = dbresponse.rows;
+      console.log(response);
+      console.log("-----------------------------")
+      const check = json(response);
+      console.log("-----------------------------")
+      console.log(check);
+      res.render("collection.ejs", { posts: response.data });
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching posts" });
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
